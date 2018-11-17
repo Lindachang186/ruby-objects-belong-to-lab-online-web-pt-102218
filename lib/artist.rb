7a05484eee654fe5a57aac53ba17a01c97d30578
Linda Chang <lindachang186@gmail.com>
2018-11-17 15:34:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,13 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song
 
   def initialize(name)
     @name = name
   end
-  
+
 end
 
 drake = Artist.new("Drake", "rap")
 hotline_bling = Song.new("Hotline Bling")
- 
+
 hotline_bling.artist = drake
